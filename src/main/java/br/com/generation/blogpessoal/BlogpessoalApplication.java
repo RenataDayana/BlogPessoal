6c285c7867643540db825fce0b174c25018348c1
renataintec <renata.dayana@gmail.com>
2021-07-27 21:34:07 -0300

    BlogPessoal

--- src/main/java/br/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/br/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -2,6 +2,8 @@ package br.com.generation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class BlogpessoalApplication {
